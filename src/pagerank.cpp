79b7e92e690321de6efeefefd0275044bd016ad6
annasehgal <annasehgal05@gmail.com>
2025-12-06 21:31:18 -0800

    chore: basic structure cpp

--- src/pagerank.cpp
+++ src/pagerank.cpp
@@ -1,5 +1,11 @@
-#include <iostrea>
+// Name: Anna Sehgal
+/* This project covers the implementation of Pagerank*/
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
 int main (){
   return 0;
 }
