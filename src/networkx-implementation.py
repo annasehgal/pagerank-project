7828a15bf7146a56f7107800110b95398a81d7bb
annasehgal <annasehgal05@gmail.com>
2025-12-07 22:46:14 -0800

    feat: implemented with networkx

--- src/networkx-implementation.py
+++ src/networkx-implementation.py
@@ -5,13 +5,14 @@
 
 from pathlib import Path
 import networkx
+import glob
 
 #this is to read .in files in test-python
 def readin(file_path):
     with open(file_path, "r") as i:
         totalnodes = int(i.readline())
         totaledges = int(i.readline())
-        edges = [tuple(map(int, line.split())) for line in f]
+        edges = [tuple(map(int, line.split())) for line in i]
     return totalnodes, totaledges, edges
 
 #this write .out files in test-python using networkx library
@@ -21,12 +22,20 @@ def writeout(file_path, pr_dict):
             o.write(f"Node {node+1}: {rank:.6f}\n")
 
 def networkxpagerankimplement(inp, outs, damping=0.85):
+    totalnodes, totaledges, edges = readin(inp)
+    G = networkx.DiGraph()
+    G.add_nodes_from(range(totalnodes))
+    G.add_edges_from([(u-1, v-1) for u,v in edges])
+    pr = networkx.pagerank(G, alpha=damping, max_iter=1000, tol=1e-6)
+    writeout(outs, pr)
+
 
 def main():
     tests = Path("../tests-python")
-    for in inp sorted(tests.glob(*.in)):
+    # for in inp sorted(tests.glob(*.in)):
+    for inp in tests.glob("*.in"):
         outs = inp.with_suffix(".out")
         print(f"output {inp} -> {outs}")
-        networkxpagerankimplement(inp, out)
+        networkxpagerankimplement(inp, outs)
 if __name__ == "__main__":
     main()
