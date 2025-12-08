7afe05ffd9e873c91e7a75fd1eecf3fe8372dd35
annasehgal <annasehgal05@gmail.com>
2025-12-08 07:18:13 -0800

    test out

--- src/util.cpp
+++ src/util.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <iomanip>
 #include "util.h"
 using namespace std;
 
@@ -9,16 +10,16 @@ using namespace std;
 void Util::builder(Graph& gr){
     int nodes;// from input number of pages
     int edges; // from input number of total edges
-    cout << "No of nodes:";
+    // cout << "No of nodes:";
     cin >> nodes;
     int start =0; //insertion -> graph
     while(start < nodes){ //loop adding
         gr.insertnode(start +1);
         start++;
     }
-    cout <<"no of edges:";
+    // cout <<"no of edges:";
     cin >> edges;
-    cout << "input egdes (start end) format: ";
+    // cout << "input egdes (start end):";
     int e = 0; // add edges->graph
     while (e < edges) {
         int u; //start
@@ -31,13 +32,23 @@ void Util::builder(Graph& gr){
 
 }
 // rank Node x : xxx.xx value
+// void Util::ranker(const Graph& gr){
+//     const vector<shared_ptr<Node>>& pages = gr.findedge();
+//     cout << fixed << setprecision(6);
+//     cout << "\nPager scores for ranks:" << endl;
+//     size_t start = 0;
+//     while (start < pages.size()){
+//         shared_ptr<Node> pg = pages[start];
+//         cout << "Node " << pg->nodeID() << ": " << pg->ranknow() << endl;
+//         start++;
+//     }
+// }
 void Util::ranker(const Graph& gr){
-    const vector<shared_ptr<Node>>& pages = gr.findedge();
+    const auto& pages = gr.findedge();
     cout << "\nPager scores for ranks:" << endl;
-    size_t start = 0;
-    while (start < pages.size()){
-        shared_ptr<Node> pg = pages[start];
-        cout << "Node " << pg->nodeID() << ": " << pg->ranknow() << endl;
-        start++;
+    for(const auto& pg : pages){
+        double val = pg->ranknow();
+        val = round(val * 1e6) / 1e6; // proper rounding
+        cout << "Node " << pg->nodeID() << ": " << fixed << setprecision(6) << val << endl;
     }
 }
