209561beef4cb771c3b8b6c0c8ab8276847ca6e5
annasehgal <annasehgal05@gmail.com>
2025-12-08 04:42:23 -0800

    simulatikon of main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,17 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory>
 #include <algorithm>
+#include "graph.h"
+#include "pagerank.h"
+#include "util.h"
 using namespace std;
 int main (){
+  Graph gr;
+  Util::builder(gr); // make the graph
+  Pagerank runpagerank(0.85, 1e-6, 100); // on damping factor 0.85 , convergence, max 
+  runpagerank.simulate(gr);
+  Util::ranker(gr); // score of each node on graph
   return 0;
 }
