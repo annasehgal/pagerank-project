19ed911b8c7e84283a0a6e62f46e3b90ee373214
annasehgal <annasehgal05@gmail.com>
2025-12-08 02:08:11 -0800

    node for signle page

--- src/Node.cpp
+++ src/Node.cpp
@@ -7,41 +7,56 @@ using namespace std;
 Node::Node(int id) : id(id), scorenow(1.0), scorenext(0.0) {
 }
 
+// get the id of node
 int Node::nodeID() const {
     return id;
 }
 
+// its current rank on that iteration
 double Node::ranknow() const {
     return scorenow;
 }
 
+// on the next iteration updated rank
 double Node::ranknext() const {
     return scorenext;
 }
 
+// outside arrows from page
 const vector<int>& Node::nodeout() const {
-
+    return outarrow;
 }
 
-
+// towards arrow to page
 const vector<int>& Node::nodein() const{
-
+    return inarrow;
 }
+
+// adds as it goes
 void Node::arrowoutside(int v){
+    outarrow.push_back(v);
 
 }
 void Node::arrowin(int u){
+    outarrow.push_back(u);
 
 }
+
+// score updates
 void Node::setterrank(double score){
+    scorenow=score;
 
 }
 void Node::setternextrank(double score){
+    scorenext = score;
 
 }
+
+// next score is pushed
 void Node::addnextiterationrank(){
+    scorenow = scorenext;
 
 }
 void Node::resetaddnextscore(){
-    
+    scorenext = 0.0;
 }
